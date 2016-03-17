c10f00e7fb6de80643dcbb2c61ddddf0e8696eb7
urosmeh <uros.meh@gmail.com>
2016-03-17 08:34:35 +0100

    gumbi

--- footer.php
+++ footer.php
@@ -4,12 +4,12 @@
 			<ul>
 				<li>
 					<h2>Meni</h2>
-					<ul>
-						<li><button class="btn btn-primary"><a href="ad_add.php">Objavi oglas</a></button></li>
-						<li><a href="ad_list.php">Vsi oglasi</a></li>
-						<li><a href="my_offers.php">Moji oglasi</a></li>
-						<li><a href="categories_list.php">Preglej kategorije</a></li>
-						<li><a href="my_auctions.php">Oddane ponudbe</a></li>
+					<ul id="list123">
+						<li id="list123"><button id="gumb" class="btn btn-primary"><a href="ad_add.php">Objavi oglas</a></button></li>
+						<li><button id="gumb" class="btn btn-primary"><a href="ad_list.php">Vsi oglasi</a></button></li>
+						<li><button id="gumb" class="btn btn-primary"><a href="my_offers.php">Moji oglasi</a></button></li>
+						<li><button id="gumb" class="btn btn-primary"><a href="categories_list.php">Preglej kategorije</a></button></li>
+						<li><button id="gumb" class="btn btn-primary"><a href="my_auctions.php">Oddane ponudbe</a></button></li>
 						<!--<li><a href="#">Urnanet non molestie semper</a></li>
 						<li><a href="#">Proin gravida orci porttitor</a></li>-->
 
