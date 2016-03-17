398b27f36021973e1d8bc74caebdc61a069b86fa
slemi98 <drdee321@gmail.com>
2016-03-17 01:42:17 +0100

    urejanje prijavne strani

--- login.php
+++ login.php
@@ -3,6 +3,8 @@
 ?>
 
 <form class="form" action="login_check.php" method="post" onsubmit="return formCheck(this)">
+<b><h3> Pozdravljeni na strani Zajec!</h3>
+<h4>Za nadaljevanje se <u>morate prijaviti v sistem</u> </h4> </b>
    <div class="input-group">
        <label>Email</label>
        <input class="form-control" type="text" name="email" />
